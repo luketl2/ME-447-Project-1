dfd19d40d5f03a2b933f17b32ae4cebfc856916c
Bhargav Chandaka <bhargav2900@gmail.com>
2021-10-25 11:50:42 -0500

    Got GA Class working with discrete problem

--- code/common/GeneticAlgorithm.py
+++ code/common/GeneticAlgorithm.py
@@ -1,45 +1,73 @@
 import numpy as np
-import time 
+import time
 
+from numpy.lib.function_base import select 
+
+def default_individual_constraint(pop):
+    return 
 class GeneticAlgorithm:
     def __init__(self, param_size: int, pop_size: int, low_bound: int, high_bound: int,
-                fitness_func, discrete=False):
+                fitness_func, constraint_func, discrete=False):
         self.param_size = param_size
         self.pop_size = pop_size
         self.low_bound = low_bound
         self.high_bound = high_bound
+        self.initial_high_bound = high_bound
         self.fitness_func = fitness_func # input is total_pop
-
+        self.environmental_constraint = constraint_func
         self.discrete = discrete
         self.n_mating = pop_size//2
         self.crossover_idx = param_size//2
         self.mutation_rate = 0.5
+        self.mutation_center = 0
         self.mutation_delta = 0.5
-
+        
+        self.best_solution = np.array([])
         self.best_outputs = []
         self.runtime = -1
+
+    def create_random_pop(self, num_indivuals):
+        population = np.random.uniform(
+            low = self.low_bound, 
+            high = self.initial_high_bound, 
+            size=(num_indivuals, self.param_size))
+        if self.discrete:
+            mu = 0.0 # mean
+            sigma = 0.3 # st. dev, spread
+            # population = np.random.normal(loc = mu, scale=sigma, size = (num_indivuals, self.param_size))
+            
+            population = np.round(population)
+        assert population.shape == (num_indivuals, self.param_size)
+        return population
 
     def gen_population(self):
         """
         Generates a random matrix of size (pop_size, param_size) with values in the given bounds. 
         Integer values are used if discrete = True
         """
-        population = np.random.uniform(
-            low = self.low_bound, 
-            high = self.high_bound, 
-            size=(self.pop_size, self.param_size))
-        if self.discrete:
-            population = np.round(population)
+        population = self.create_random_pop(self.pop_size)
+        population = self.environmental_constraint(population)
+        while population.shape[0] < self.pop_size:
+            print(f"Cur Shape: {population.shape[0]}")
+            cur_size = population.shape[0]
+            extra_pop = self.create_random_pop(self.pop_size-cur_size)
+            population = np.vstack((population, extra_pop))
+            population = self.environmental_constraint(population)
+
+        assert population.shape == (self.pop_size, self.param_size)
         return population
 
     def calculate_fitness(self, total_pop):
-        return self.fitness_func(total_pop)
+        fitness = self.fitness_func(total_pop)
+        assert fitness.shape == (total_pop.shape[0],)
+        return fitness
     
     def select_deterministic(self, total_pop, select_num):
         parents = np.empty((select_num, self.param_size))
         t_fitness = self.calculate_fitness(total_pop)
         best_fitness = np.argsort(t_fitness)[0:select_num]
         parents = total_pop[best_fitness]
+        assert parents.shape == (select_num, self.param_size)
         return parents
 
     def crossover(self, t_parents):
@@ -60,20 +88,29 @@ class GeneticAlgorithm:
     def mutation(self, t_offspring):
         mutated_offspring = np.copy(t_offspring)
         random_values = np.random.uniform(
-            low=-self.mutation_delta, 
-            high=self.mutation_delta, 
+            low=self.mutation_center-self.mutation_delta, 
+            high=self.mutation_center+self.mutation_delta, 
             size=t_offspring.shape
         )
         coin_toss = np.random.uniform(low=0, high=1, size=t_offspring.shape)
         ind = np.nonzero(coin_toss > self.mutation_rate)
-        print(ind)
-        print(random_values.shape)
-        mutated_offspring[ind] += random_values[ind]
+        if self.discrete:
+            mutated_offspring[ind] += np.round(random_values[ind])
+        else:
+            mutated_offspring[ind] += random_values[ind]
+        # Clip mutations between low and high bounds
+        too_low_idx = mutated_offspring < self.low_bound
+        too_high_idx = mutated_offspring > self.high_bound
+        mutated_offspring[too_low_idx] = self.low_bound
+        mutated_offspring[too_high_idx] = self.high_bound
 
+        assert ((mutated_offspring >= self.low_bound) & (mutated_offspring <= self.high_bound)).all()
         assert mutated_offspring.shape == t_offspring.shape
         return mutated_offspring
     
     def environmental_selection(self, curr_population, offspring):
+        offspring = self.environmental_constraint(offspring)
+        print(f"Offspring: {offspring}")
         t_total_pop = np.vstack((curr_population, offspring))
         new_pop = self.select_deterministic(t_total_pop, self.pop_size)
         assert new_pop.shape == (self.pop_size, self.param_size)
@@ -87,7 +124,7 @@ class GeneticAlgorithm:
         start_time = time.time()
         for generation in range(num_generations):
             print(f"Generation : {generation}")
-            print(f"Current Population: {curr_population}")
+            # print(f"Current Population: {curr_population}")
             fitness = self.calculate_fitness(curr_population)
             min_fitness = np.min(fitness)
 
@@ -104,9 +141,9 @@ class GeneticAlgorithm:
         fitness = self.calculate_fitness(curr_population)
         print(fitness)
         # Then return the index of that solution corresponding to the best fitness.
-        max_idx = np.argmin(fitness)
-
-        print("Best solution : ", curr_population[max_idx])
-        print("Best solution fitness : ", fitness[max_idx])
+        min_idx = np.argmin(fitness)
+        self.best_solution = curr_population[min_idx]
+        print("Best solution : ", curr_population[min_idx])
+        print("Best solution fitness : ", fitness[min_idx])
         end_time = time.time()
         self.runtime = end_time-start_time
